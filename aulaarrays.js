4f443d5c6392bcddd50b83018a5dbe24d057063d
Samira Silva <iasamirasilva@gmail.com>
2022-01-01 17:02:55 -0300

    Initial commit

---
+++ aulaarrays.js
@@ -0,0 +1,8 @@
+//Arrays são um conjunto de dados q podem ser acessados por um índice
+
+let familia = [34, 33, 32];
+
+//acessar valor do índice ex: (familia [1]); indice inicia com 0
+
+console.log(familia[1]);
+
