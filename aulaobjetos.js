5986018a9e49786fa4ffe12b0c75db1e64b98a6a
Samira Silva <iasamirasilva@gmail.com>
2022-01-01 16:36:34 -0300

    Initial commit

---
+++ aulaobjetos.js
@@ -0,0 +1,13 @@
+let nome = 'Noeli';
+let idade = 45;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome: 'Noeli',
+    idade: 45,
+    estaAprovado: true,
+    sobrenome : 'dos Santos'
+};
+
+console.log(pessoa);
