7b9b51110bd83c712450a4ed90dc4c7e5bee163f
Samira Silva <iasamirasilva@gmail.com>
2022-01-01 17:42:28 -0300

    Initial commit

---
+++ aulatiposdefuncoes.js
@@ -0,0 +1,17 @@
+// 1 funcao, realiza 1 tarefa e nao devolve nada
+function dizerNome(){
+    console.log('Samira')
+}
+
+dizerNome();
+//faz cálculo ou operação e retorna algo
+function MultiplicarPorDois(valor){
+    return valor* 2;
+}
+
+console.log(MultiplicarPorDois(5));
+
+let resultado = MultiplicarPorDois(5);
+
+console.log(resultado);
+//
