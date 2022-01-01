14c834e4bebdec1687d00c6d7442b7fc2a946a10
Samira Silva <iasamirasilva@gmail.com>
2022-01-01 16:10:05 -0300

    Initial commit

---
+++ aulatiposprimitivos.js
@@ -0,0 +1,5 @@
+let nome = 'Samira'; //tipo referência 'string literal'
+let idade = 34; // tipo referência 'number literal'
+let  aptoAptovado = true; // tipo referência 'boolean'- cria regras
+let sobrenome = underfined; //undefined  
+let corSelecionada = null; // redefinir valor da variável
